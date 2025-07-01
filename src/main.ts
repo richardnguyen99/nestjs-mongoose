7fc0f6b618403c82c97558ce065842dffa7aeab6
Richard Nguyen <richard@richardhnguyen.com>
2025-06-30 22:16:31 -0700

    feat: add essential files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  let a = "Hello, World!";
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
