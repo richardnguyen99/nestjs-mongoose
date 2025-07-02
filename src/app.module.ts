37f4e02d02df166efa545a41d4f04baf6aad4a23
Richard Nguyen <richard@richardhnguyen.com>
2025-07-01 20:58:32 -0700

    feat: add Mongoose Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>("MONGODB_URI") || "mongodb://localhost:27018",
+        dbName: config.get<string>("MONGODB_DB_NAME") || "tmdb",
+        user: config.get<string>("MONGODB_USER") || "admin",
+        pass: config.get<string>("MONGODB_PASSWORD") || "admin",
+        ssl: config.get<string>("NODE_ENV") === "production",
+        directConnection: true,
+      }),
+
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
